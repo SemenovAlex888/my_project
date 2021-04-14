9e23598192d40c76226759a83b335d16463090d0
SemenovAlex888 <aurum25@ya.ru>
2021-04-14 13:23:48 +0300

    04_add_DataJpaUserRepository

---
+++ src/main/java/ru/my_project/repository/DataJpaUserRepository.java
@@ -0,0 +1,25 @@
+package ru.my_project.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import ru.my_project.model.User;
+
+@Repository
+@Transactional(readOnly = true)
+public interface DataJpaUserRepository extends JpaRepository<User, Integer> {
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM User u WHERE u.id=:id")
+    int delete(@Param("id") int id);
+
+    User getByEmail(String email);
+
+    @Transactional
+    @Override
+    User save(User user);
+}
