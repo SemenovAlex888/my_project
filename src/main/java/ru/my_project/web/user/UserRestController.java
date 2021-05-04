34913d32cbc315609adf18dce8afb618f5d388de
SemenovAlex888 <aurum25@ya.ru>
2021-05-04 15:41:27 +0300

    15_add_method_register_User

--- src/main/java/ru/my_project/web/user/UserRestController.java
+++ src/main/java/ru/my_project/web/user/UserRestController.java
@@ -2,8 +2,12 @@ package ru.my_project.web.user;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import ru.my_project.model.User;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = UserRestController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -27,5 +31,14 @@ public class UserRestController extends AbstractUserController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void update(@RequestBody User user) {
         super.update(user, ID);
+    }
+
+    @PostMapping(value = "/register", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<User> register(@RequestBody User user) {
+        User created = super.create(user);
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(REST_URL).build().toUri();
+        return ResponseEntity.created(uriOfNewResource).body(created);
     }
 }
